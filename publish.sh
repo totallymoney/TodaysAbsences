c45ad3dc6b28efc2d66f7adc25c0db0f7eb878d5
Edward Dewhurst <edward@dewhurst.io>
2018-01-29 19:32:57 +0000

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -1,3 +1,6 @@
-cd src/TodaysAbsences.Lambda/bin/Release/netcoreapp2.0/publish
-zip -r ../../../../../../lambda.zip *
-cd ../../../../../../
+root="$(pwd)"
+cd src/TodaysAbsences.Lambda
+dotnet publish -c Release
+cd bin/Release/netcoreapp2.0/publish
+zip -r "$root/lambda.zip" *
+cd "$root"
