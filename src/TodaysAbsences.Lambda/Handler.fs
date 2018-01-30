cf60795e8f7cbe1d86ab2fb5f3576105ec21103f
Edward Dewhurst <edward@dewhurst.io>
2018-01-30 14:40:34 +0000

    Add implementation of lambda handler function

--- src/TodaysAbsences.Lambda/Handler.fs
+++ src/TodaysAbsences.Lambda/Handler.fs
@@ -2,11 +2,34 @@ module Handler
 
 
 open Amazon.Lambda.Core
+open PeopleHrApi.Http
+open SlackApi.Http
 
 
 [<assembly: LambdaSerializer(typeof<Amazon.Lambda.Serialization.Json.JsonSerializer>)>]
 do ()
 
 
-let handler (input:string, _:ILambdaContext) = input.ToUpper()
+let environment = System.Environment.GetEnvironmentVariables ()
 
+
+let getEnvVarOrThrowIfMissing key =
+    if not (environment.Contains key) then
+        invalidOp (sprintf "The environment varaible %s was not found" key)
+    else
+        environment.Item key :?> string
+
+
+let private sendTodaysAbsencesMessage apiKey webhookUrl =
+    getAbsences apiKey |> Result.bind (sendMessage webhookUrl)
+
+
+let handler (_:string, _:ILambdaContext) =
+    let peopleHrApiKey = getEnvVarOrThrowIfMissing "PEOPLEHR_API_KEY"
+    let slackWebhookUrl = getEnvVarOrThrowIfMissing "SLACK_WEBHOOK_URL"
+
+
+    sendTodaysAbsencesMessage peopleHrApiKey slackWebhookUrl
+    |> function
+    | Ok message -> sprintf "Ok: %s" message
+    | Error message -> sprintf "Error: %s" message
