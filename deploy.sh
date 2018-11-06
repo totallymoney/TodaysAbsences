c1f69b4c494d661ff0a950032631fd21c6f5a422
Imre Turi <ituri@totallymoney.com>
2018-11-06 13:03:34 +0000

    Add environment validation in order to prevent us from future errors

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,15 @@ if [ $# -ne 1 ]; then
     echo "$0": eg: deploy.sh stage
     echo "$0": eg: deploy.sh production
     exit 1
+else 
+    case $1 in
+     stage|production) 
+        ;;
+     *)
+        echo "$0": ENVIRONMENT should be "stage" or "production"
+        exit 1
+        ;;
+    esac
 fi
 
 ENVIRONMENT=$1
