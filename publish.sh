f932caa4b70311f6c86c63b7fc435c7f3e2eb6ec
Imre Turi <ituri@totallymoney.com>
2019-03-25 10:24:23 +0000

    Update .net core version to 2.1

--- publish.sh
+++ publish.sh
@@ -2,6 +2,6 @@ mkdir -p publish
 root="$(pwd)"
 cd src/TodaysAbsences.Lambda
 dotnet publish -c Release
-cd bin/Release/netcoreapp2.0/publish
+cd bin/Release/netcoreapp2.1/publish
 zip -r "$root/publish/deploy-package.zip" *
 cd "$root"
